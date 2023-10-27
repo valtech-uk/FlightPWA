afd1fc4cfeb4d5235965907c4ef37cbce5b94778
Brandon Murphy <brandonmurphy@gmail.com>
2023-10-27 11:53:24 +0100

    Fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import LandingPage from './LandingPage';
-
 
 function App() {
   return (
@@ -10,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload brandon.
+          Edit <code>src/App.tsx</code> and save to reload brando.
         </p>
         <a
           className="App-link"
