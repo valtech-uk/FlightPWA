c3cf43808ce1a3c6ff19629357e4e472fcf250a5
Brandon Murphy <brandonmurphy@gmail.com>
2023-10-27 11:36:37 +0100

    Added in the landing page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import LandingPage from './LandingPage';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <LandingPage />
+        </div>
+    );
+};
 
 function App() {
   return (
---
+++ src/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+    return (
+        <div className="landing-page">
+            <h1>Welcome to My App</h1>
+            <button className="register-btn">Register</button>
+            <button className="login-btn">Login</button>
+        </div>
+    );
+};
+
+export default LandingPage;
