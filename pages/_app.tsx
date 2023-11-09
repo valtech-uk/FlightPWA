b0f83e96d7ce54fa1a00b4181c6f1333ff5b8e2e
andrii.pokrason <andrewlike228@gmail.com>
2023-11-09 11:20:32 +0100

    add prompt to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,28 @@
+"use client";
 import type { AppProps } from "next/app";
 import "../styles/transitions.module.css";
 import Transition from "../components/transitions";
 import HeadComp from "../components/general/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+declare const window: any;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const [history, setHistory] = useState([router.asPath]);
+
+  const [deferredPrompt, setDeferredPrompt] = useState<any>();
+
+  useEffect(() => {
+    const deferredPrompt = window.app?.getInstallPrompt();
+    setDeferredPrompt(deferredPrompt);
+  }, []);
+
+  const handleInstallPrompt = async () => {
+    if (deferredPrompt) {
+      await deferredPrompt.prompt();
+    }
+  };
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
@@ -24,6 +39,9 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     <Transition>
       <HeadComp />
       <Component {...pageProps} key={router.asPath} history={history} />
+      <button onClick={handleInstallPrompt} id="install-app">
+        Download
+      </button>
     </Transition>
   );
 }
