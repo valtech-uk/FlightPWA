25a077a026a73e54bdab49179fdb6988c4ca133b
vadym.melnyk <vadym.melnyk@valtech.com>
2023-11-10 14:22:01 +0200

    fixed registration types

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,14 @@
 import { ReactHTML } from 'react';
+import {BeforeAuthCallback, OnSuccessCallback} from "@passageidentity/passage-elements";
 
 interface IntrinsicElements {
     // 'passage-auth': ReactHTML['div'];
     'passage-auth': ReactHTML['div'] & {
         appId?: string;
+        onSuccess?: OnSuccessCallback
+        beforeAuth?: BeforeAuthCallback
+        lang: string,
+        defaultCountryCode?: string,
+        theme?: 'light' | 'dark' | 'auto',
     };
 }
