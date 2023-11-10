ea9d75a9d2e73062625a6457777658bc693c3e83
vadym.melnyk <vadym.melnyk@valtech.com>
2023-11-10 13:08:42 +0200

    fixed registration types

--- pages/registerLogin.tsx
+++ pages/registerLogin.tsx
@@ -1,18 +1,24 @@
-'use client'
-
-import { FunctionComponent, useEffect } from "react";
+import { FunctionComponent, useEffect, useRef } from "react";
+import { PassageElement } from '@passageidentity/passage-elements';
 
 const RegisterLogin: FunctionComponent = () => {
+    const passageAuthRef = useRef<PassageElement | null>(null);
 
-    useEffect(()=>{
-        require('@passageidentity/passage-elements/passage-auth');
+    useEffect(() => {
+        import('@passageidentity/passage-elements/passage-auth').then(() => {
+            const passageAuth = passageAuthRef.current;
+
+            if (passageAuth) {
+                passageAuth.appId = "gk74rC4fq8KS4tdh2LyyuNpi";
+            }
+        });
     }, []);
 
     return (
         <>
-            <passage-auth app-id={"gk74rC4fq8KS4tdh2LyyuNpi"}></passage-auth>
+            <passage-auth ref={passageAuthRef}></passage-auth>
         </>
-    )
-}
+    );
+};
 
 export default RegisterLogin;
